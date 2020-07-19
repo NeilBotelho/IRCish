bc87253b68c3d1d5675f12492dee2eb16ed3b2a9
Neil Botelho <neil.botelho321@gmail.com>
2020-07-20 02:35:07 +0530

    Add websocket functionality
    Able to establish a connection with a client. Tested with index.html

--- main.go
+++ main.go
@@ -8,41 +8,53 @@ import (
 	"os"
 	"time"
 
+	"github.com/gorilla/websocket"
+
 	"github.com/gorilla/mux"
 )
 
 var PORT string = "8000"
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func main() {
 	if len(os.Args) > 1 {
 		PORT = os.Args[1]
 	}
 
-	r := mux.NewRouter() //.StrictSlash(false)
+	r := mux.NewRouter() // maybe set .StrictSlash(false)?
 	srv := &http.Server{
 		// Wrap mux in a timeout Handler
-		Handler: http.TimeoutHandler(r, 10*time.Second, "Timeout!\n"),
+		Handler: r,
 		Addr:    "127.0.0.1:" + PORT,
-		// Enforce timeouts for server!
+		// Enforce timeouts for server
+		// Will set longer when sockets implemented
 		WriteTimeout: 10 * time.Second,
 		ReadTimeout:  10 * time.Second,
 		IdleTimeout:  15 * time.Second,
 	}
-
-	r.HandleFunc("/test", HomeHandler2)
-	r.HandleFunc("/{hello}", HomeHandler)
-	// r.HandleFunc("/products", ProductsHandler)
-	// r.HandleFunc("/articles", ArticlesHandler)
+	r.HandleFunc("/ws", wsHandler)
+	r.HandleFunc("/test", HomeHandler)
 	log.Print("Server running on " + PORT)
 	log.Fatal(srv.ListenAndServe())
 
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(5 * time.Second)
 	fmt.Fprintf(w, "Hello at %q", html.EscapeString(r.URL.Path))
 }
-func HomeHandler2(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(5 * time.Second)
-	fmt.Fprintf(w, "test at %q", html.EscapeString(r.URL.Path))
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Hello")
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(conn)
 }
