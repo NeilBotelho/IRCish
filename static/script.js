58675032166da2365f91a7e57dad8688a0aea505
Neil Botelho <neil.botelho321@gmail.com>
2020-07-29 21:50:17 +0530

    Edited help message

--- static/script.js
+++ static/script.js
@@ -9,7 +9,7 @@ var inputField=document.getElementById('input-field')//used for clearing input f
 var currentMessages
 var currentRoom
 var help="AVAILABLE COMMANDS:\n\n"+
-"/join channelName : to join a channel(name must contain only lowercase letters, numbers and underscores. Room names must be between 2 and 10 characters long)\n\n"+
+"/join channelName : to join a room. Room names must contain only lowercase letters, numbers and underscores. Room names must be between 2 and 10 characters long)\n\n"+
 "/identify username : to change how you are identified to 'username'. Usernames can contain any case letters, numbers and underscores. Usernames must be between  2 and 10 characters long. Usernames are not unique\n\n"+
 "/clear : clears all messages from current room"
 
